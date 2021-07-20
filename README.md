5da3c90772315cf0b32db0c860c066f659345477
unknown <orlovdimas92@mail.ru>
2021-07-21 00:40:16 +0300

    по моему не хедлесс

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/i4cjxh99m1whdvc6?svg=true)](https://ci.appveyor.com/project/Orlov-D/aqa-2-1-1-headless-selenide)
---
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -0,0 +1,41 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTest {
+//    java -jar artifacts\app-order.jar
+
+//    @BeforeAll
+//    void setUp(){
+//        $(chrome --headless --remote-debugging-port=9222 chromium.org);
+//    }
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldPass() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Васин-Красин Василь");
+        form.$("[data-test-id=phone] input").setValue("+71234567890");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+}
