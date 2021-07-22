7c98cf3613f145a1839bed2798bc4e5149f475e9
unknown <orlovdimas92@mail.ru>
2021-07-23 00:39:21 +0300

    plus dz2

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,12 +1,10 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
 //    java -jar artifacts\app-order.jar
@@ -31,6 +29,36 @@ public class CardOrderTest {
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
         $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+//    Задание 2.
+@Test
+void NameValidation() {
+    open("http://localhost:9999");
+    SelenideElement form = $("[id=root]");
+    form.$("[data-test-id=name] input").setValue("5");
+    form.$("[role=button]").click();
+    $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+}
+
+    @Test
+    void PhoneValidation() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("5");
+        form.$("[role=button]").click();
+        $$(".input__sub").get(1).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void CheckboxValidation() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79012345678");
+        form.$("[role=button]").click();
+        $(".input_invalid ").exists();
     }
 
 }
