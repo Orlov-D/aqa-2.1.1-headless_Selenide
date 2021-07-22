7aa71c064fd9110a43bdcac2a77fc2a0c0035f39
unknown <orlovdimas92@mail.ru>
2021-07-23 00:21:19 +0300

    совсем не хедлесс и экзист

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -5,16 +5,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
 //    java -jar artifacts\app-order.jar
-
-//    @BeforeAll
-//    void setUp(){
-//        $(chrome --headless --remote-debugging-port=9222 chromium.org);
-//    }
 
     @Test
     void shouldSubmitRequest() {
@@ -28,14 +25,32 @@ public class CardOrderTest {
     }
 
     @Test
-    void shouldPass() {
+    void NameValidation() {
         open("http://localhost:9999");
         SelenideElement form = $("[id=root]");
-        form.$("[data-test-id=name] input").setValue("Васин-Красин Василь");
-        form.$("[data-test-id=phone] input").setValue("+71234567890");
-        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=name] input").setValue("5");
         form.$("[role=button]").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void PhoneValidation() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("5");
+        form.$("[role=button]").click();
+        $$(".input__sub").get(1).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void CheckboxValidation() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79012345678");
+        form.$("[role=button]").click();
+        $(".input_invalid ").exists();
     }
 
 }
