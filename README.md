f1d8487a19574f277f0ee335af5fae67e1c1d0e3
unknown <orlovdimas92@mail.ru>
2021-07-24 00:47:13 +0300

    11 правок

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/i4cjxh99m1whdvc6?svg=true)](https://ci.appveyor.com/project/Orlov-D/aqa-2-1-1-headless-selenide)
+
+<!--
+java -jar artifacts\app-order.jar
+-->
--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,64 +1,84 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
-//    java -jar artifacts\app-order.jar
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:9999");
+    }
 
     @Test
-    void shouldSubmitRequest() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[id=root]");
+    public void shouldSubmitRequest() {
+        SelenideElement form = $("[method=post]");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
     @Test
-    void shouldPass() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[id=root]");
+    public void shouldPass() {
+        SelenideElement form = $("[method=post]");
         form.$("[data-test-id=name] input").setValue("Васин-Красин Василь");
         form.$("[data-test-id=phone] input").setValue("+71234567890");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $(".Success_successBlock__2L3Cw").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
-//    Задание 2.
-@Test
-void NameValidation() {
-    open("http://localhost:9999");
-    SelenideElement form = $("[id=root]");
-    form.$("[data-test-id=name] input").setValue("5");
-    form.$("[role=button]").click();
-    $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
-}
+    @Test
+    public void nameValidationWithValue() {
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("5");
+        form.$("[data-test-id=phone] input").setValue("+71234567890");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
 
     @Test
-    void PhoneValidation() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[id=root]");
+    public void nameValidationEmpty() {
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=phone] input").setValue("+71234567890");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void phoneValidationWithValue() {
+        SelenideElement form = $("[method=post]");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("5");
+        form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
-        $$(".input__sub").get(1).shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
-    void CheckboxValidation() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[id=root]");
+    public void phoneValidationEmpty() {
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void checkboxValidation() {
+        SelenideElement form = $("[method=post]");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79012345678");
         form.$("[role=button]").click();
-        $(".input_invalid ").exists();
+        $("[data-test-id=agreement].input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
     }
 
 }
